54c29b23cf6c69782994ab381190f85378a54093
Corwin96 <corwinvandalen@gmail.com>
2022-09-22 18:51:20 +0200

    Message event handler & user schema

---
+++ src/schemas/user.js
@@ -0,0 +1,12 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  _id: Schema.Types.ObjectId,
+  userID: { type: String },
+  guildID: { type: String },
+  xp: { type: Number, default: 0 },
+  messageCount: { type: Number, default: 0 },
+  lastUpdated: { type: Date, default: new Date() },
+});
+
+module.exports = model("User", userSchema, "users");
